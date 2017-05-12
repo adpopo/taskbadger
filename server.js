b1332f88d44ae88610c52812c46bab696451d82c
adpopo <hxuanpham@gmail.com>
2017-05-11 18:50:05 -0700

    more login stuff

--- server.js
+++ server.js
@@ -4,13 +4,26 @@
 
 // require('dotenv').config({path: __dirname + '/../.env'});
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const RedisStore = require('connect-redis')(session);
 const sms = require('./controllers/smsController.js');
 var schedule = require('node-schedule');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({  
+  store: new RedisStore({
+    url: config.redisStore.url
+  }),
+  secret: config.redisStore.secret,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize()); 
+app.use(passport.session());
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
