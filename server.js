57d9e7e7eaf3d36e4e2400d8e605e7691a7b8e9c
Hoang <hxuanpham@gmail.com>
2017-05-12 01:39:19 +0000

    thursday

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 'use strict';
 
 // dependencies
-require('dotenv').config({path: __dirname + '/../.env'});
 
+// require('dotenv').config({path: __dirname + '/../.env'});
 const express = require('express');
 const bodyParser = require('body-parser');
 const sms = require('./controllers/smsController.js');
+var schedule = require('node-schedule');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,16 +15,20 @@ const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
-// sending Badger SMS message
-app.post('/send', (req, res) => {
-  // Sending SMS via Nexmo
-  sms.message.sendSms(
-    process.env.FROM_NUMBER, req.body.toNumber, req.body.message, {type: 'unicode'},
-    (err, responseData) => {
-    	if (responseData) {
-	    	console.log(responseData);
+// constructor function? accept time, 
+function badger(FROM_NUMBER, toNumber, message, time) {
+  
+  // sending Badger SMS message
+  app.post('/send', (req, res) => {
+    // Sending SMS via Nexmo
+    sms.message.sendSms(
+      process.env.FROM_NUMBER, req.body.toNumber, req.body.message, {type: 'unicode'},
+      (err, responseData) => {
+    	  if (responseData) {
+	    	  console.log(responseData);
 	    }
-        res.end(JSON.stringify(responseData));
-	}
-  );
-});
+          res.end(JSON.stringify(responseData));
+	    }
+    );
+  });
+};
